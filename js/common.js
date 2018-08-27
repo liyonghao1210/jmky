d67733024b51762e3ec82afe3bcb48040a28951c
liyonghao1210 <lyh_md@163.com>
2018-08-27 17:29:40 +0800

    商品管理下拉框

---
+++ js/common.js
@@ -0,0 +1,8 @@
+/*导入头部和尾部*/
+$(document).ready(function(){
+    $(".layui-header").load("header.html");
+    $(".leftNav").load("leftNav.html");
+    $(".layui-footer").load("footer.html");
+})   
+
+    
